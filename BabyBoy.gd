1f75d43147136e7c00a595342d330ef78783a794
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-04-20 10:41:45 -0400

    Enemy Work
    Adding some of the basics from Tyler and making the enemy randomly move around.

---
+++ BabyBoy.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+var timer = Timer.new()
+var rng = RandomNumberGenerator.new()
+
+const GRAVITY = 200.0
+const WALK_SPEED = 200
+const JUMP_SPEED = 100
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+	var state = rng.randi_range(0, 3)
+	print(state)
+	
+	# Stay still
+	if state == 0:
+		pass
+		
+	# Right
+	elif state == 1:
+		velocity.x = -WALK_SPEED
+		
+		
+	# Left
+	elif state == 2:
+		velocity.x = WALK_SPEED
+		
+	# Jump
+	elif state == 3 and is_on_floor():
+		velocity.y = -JUMP_SPEED
+
+	move_and_slide(velocity, Vector2(0, -1))
--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 class_name Actor
 
+var timer = Timer.new()
+
 const GRAVITY = 200.0
 const WALK_SPEED = 200
+const JUMP_SPEED = 900
 
 var velocity = Vector2()
 
@@ -16,10 +19,4 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 
-	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
 	move_and_slide(velocity, Vector2(0, -1))
-
-
