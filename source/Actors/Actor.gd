453dc5ee4d39605597b1ecbc858ce0a3c9bdb6d8
Tsiegmund <Tyler2023411@live.ytech.edu>
2022-04-20 10:49:58 -0400

    jump ability

--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -1,25 +1,30 @@
 extends KinematicBody2D
 class_name Actor
 
-const GRAVITY = 200.0
+const GRAVITY = 30
 const WALK_SPEED = 200
+const JUMP_SPEED = 900
 
 var velocity = Vector2()
 
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
+	velocity.y = velocity.y + GRAVITY
+
+	print(velocity.y)
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
+		
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x =  WALK_SPEED
+	
+	elif Input.is_action_pressed("ui_up") and is_on_floor():
+		velocity.y = -JUMP_SPEED
+
 	else:
 		velocity.x = 0
-
-	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
+		
+	
 	move_and_slide(velocity, Vector2(0, -1))
 
 
