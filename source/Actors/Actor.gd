cbf251a9dbf9481e74df02368d342004fc895384
Tsiegmund <Tyler2023411@live.ytech.edu>
2022-04-22 10:21:39 -0400

    Player can move + shoot

--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 class_name Actor
 
+const bulletPath = preload("res://source/Other/MVPProjectile.tscn")
+
 const GRAVITY = 30
 const WALK_SPEED = 200
 const JUMP_SPEED = 900
 
 var velocity = Vector2()
+
+
 
 func _physics_process(delta):
 	velocity.y = velocity.y + GRAVITY
@@ -20,11 +24,24 @@ func _physics_process(delta):
 	
 	elif Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -JUMP_SPEED
+	
+	elif Input.is_action_just_pressed("shoot"):
+		shoot()
+		
+		$Node2D.look_at(get_global_mouse_position())
 
 	else:
 		velocity.x = 0
 		
+
+
 	
 	move_and_slide(velocity, Vector2(0, -1))
 
-
+func shoot():
+	var bullet = bulletPath.instance()
+	
+	get_parent().add_child(bullet)
+	bullet.position = $Node2D/Position2D.global_position
+	
+	bullet.velocity = get_global_mouse_position() - bullet.position
