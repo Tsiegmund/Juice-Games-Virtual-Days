c1daad14197b0332b72d59b86e62aea46ade9120
Tsiegmund <Tyler2023411@live.ytech.edu>
2022-04-26 09:59:09 -0400

    created health bars

--- BabyBoy.gd
+++ BabyBoy.gd
@@ -1,9 +1,20 @@
 extends KinematicBody2D
+class_name BabyBoy 
+
+
+signal health_updated(health)
+signal killed()
+
 var timer = Timer.new()
 var rng = RandomNumberGenerator.new()
 var GRAVITY = 30
 var WALK_SPEED = 200
 var JUMP_SPEED = 400
+
+export (float) var max_health = 100000000000
+
+onready var health = max_health setget _set_health
+
 var velocity = Vector2()
 func _physics_process(delta):
 	velocity.y = velocity.y + GRAVITY
@@ -27,4 +38,29 @@ func _physics_process(delta):
 	if state == 3 and is_on_floor():
 		velocity.y = -JUMP_SPEED
 		timer.set_wait_time(5)
+		
+		
 	move_and_slide(velocity, Vector2(0, -1))
+	
+	var label = get_node("Label")
+	label.text = str(health) + "/" + str(max_health)
+	
+	
+func damage(amount):
+	_set_health(health - amount)	
+
+	
+func kill():
+	pass
+	
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated", health)
+		var label = get_node("Label")
+		label.text = str(health) + "/" + str(max_health)
+		if health == 0:
+			self.queue_free()
+			
+		
--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -9,7 +9,9 @@ const JUMP_SPEED = 900
 
 var velocity = Vector2()
 
+export (float) var max_health = 100
 
+onready var health = max_health setget _set_health
 
 func _physics_process(delta):
 	velocity.y = velocity.y + GRAVITY
@@ -33,7 +35,8 @@ func _physics_process(delta):
 		
 		$Node2D.look_at(get_global_mouse_position())
 	
-
+	var label = get_node("Label")
+	label.text = str(health) + "/" + str(max_health)
 	
 		
 	
@@ -48,3 +51,20 @@ func shoot():
 	bullet.position = $Node2D/Position2D.global_position
 	
 	bullet.velocity = get_global_mouse_position() - bullet.position
+	
+func damage(amount):
+	_set_health(health - amount)	
+
+	
+func kill():
+	pass
+	
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated", health)
+		var label = get_node("Label")
+		label.text = str(health) + "/" + str(max_health)
+		if health == 0:
+			self.queue_free()
