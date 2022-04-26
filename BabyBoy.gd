ebe02276b8ed5600f8883aad609e5f7b1168633a
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-04-26 09:49:57 -0400

    Map design + bullet work
    Trying to make the bullet disappear after collision (not going well)

--- BabyBoy.gd
+++ BabyBoy.gd
@@ -10,12 +10,12 @@ var WALK_SPEED = 200
 var JUMP_SPEED = 400
 var velocity = Vector2()
 
-
+#MODE_KINEMATIC:
 func _physics_process(delta):
 	velocity.y = velocity.y + GRAVITY
 	if is_on_floor():
 		velocity.y = 0
-	if velocity.y >= 2000:
+	if velocity.y > 2000:
 		velocity.y = 2000
 	var state = rng.randi_range(0, 3)
 	# Stay still
@@ -29,7 +29,7 @@ func _physics_process(delta):
 	if state == 2:
 		velocity.x = WALK_SPEED
 		timer.set_wait_time(600)
-	 # Jump
+		 # Jump
 	if state == 3 and is_on_floor():
 		velocity.y = -JUMP_SPEED
 		timer.set_wait_time(5)
--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -11,10 +11,12 @@ var velocity = Vector2()
 
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	velocity.y = velocity.y + GRAVITY
-
-	print(velocity.y)
+	if velocity.y > 2000:
+		velocity.y = 2000
+	if is_on_floor():
+		velocity.y = 0
 
 	if Input.is_action_pressed("ui_left"):
 		velocity.x = -WALK_SPEED
@@ -33,12 +35,6 @@ func _physics_process(delta):
 		
 		$Node2D.look_at(get_global_mouse_position())
 	
-
-	
-		
-	
-
-	
 	move_and_slide(velocity, Vector2(0, -1))
 
 func shoot():
@@ -46,5 +42,6 @@ func shoot():
 	
 	get_parent().add_child(bullet)
 	bullet.position = $Node2D/Position2D.global_position
+	var previous_position = bullet.position
 	
 	bullet.velocity = get_global_mouse_position() - bullet.position
