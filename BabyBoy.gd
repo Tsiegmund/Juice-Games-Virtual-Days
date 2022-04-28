5c63b30548a8b3c826ba9caebb693e18e4cd317e
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-04-28 11:08:44 -0400

    Trying to save bullet

--- BabyBoy.gd
+++ BabyBoy.gd
@@ -22,22 +22,22 @@ func _physics_process(delta):
 		velocity.y = 0
 	if velocity.y > 2000:
 		velocity.y = 2000
-	#var state = rng.randi_range(0, 3)
-	# Stay still
-	#if state == 0:
+	var state = rng.randi_range(0, 3)
+#	 Stay still
+	if state == 0:
 		timer.set_wait_time(600)
 	# Right
-	#if state == 1:
-#		velocity.x = -WALK_SPEED
-#		timer.set_wait_time(600)
+	if state == 1:
+		velocity.x = -WALK_SPEED
+		timer.set_wait_time(600)
 	# Left
-	#if state == 2:
-#		velocity.x = WALK_SPEED
-#		timer.set_wait_time(600)
+	if state == 2:
+		velocity.x = WALK_SPEED
+		timer.set_wait_time(600)
 		 # Jump
-	#if state == 3 and is_on_floor():
-#		velocity.y = -JUMP_SPEED
-#		timer.set_wait_time(5)
+	if state == 3 and is_on_floor():
+		velocity.y = -JUMP_SPEED
+		timer.set_wait_time(5)
 		
 	var label = get_node("Label")
 	label.text = str(health) + "/" + str(max_health)
--- MVPProjectile.gd
+++ MVPProjectile.gd
@@ -1,17 +1,2 @@
-extends RigidBody2D
+extends KinematicBody2D
 
-var projectile_speed = 400
-var life_time = 3
-
-
-func _ready():
-	apply_impulse(Vector2(), Vector2(projectile_speed, 0).rotated(rotation))
-	SelfDestruct()
-
-func SelfDestruct():
-	yield(get_tree().create_timer(life_time), "timeout")
-	queue_free()
-
-
-func _on_Bullet_body_entered(body):
-	self.hide()
