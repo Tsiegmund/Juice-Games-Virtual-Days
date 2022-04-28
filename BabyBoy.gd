b42a502b6d1b7a9d3cf20317b227f47a88c5d78c
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-04-28 10:13:44 -0400

    Making better bullet pt. 1
    Bullet works. It collides and disappears with objects in it's surroundings, but I cannot make it link to the player.

--- BabyBoy.gd
+++ BabyBoy.gd
@@ -12,27 +12,28 @@ var velocity = Vector2()
 
 #MODE_KINEMATIC:
 func _physics_process(delta):
+	
 	velocity.y = velocity.y + GRAVITY
 	if is_on_floor():
 		velocity.y = 0
 	if velocity.y > 2000:
 		velocity.y = 2000
-	var state = rng.randi_range(0, 3)
+	#var state = rng.randi_range(0, 3)
 	# Stay still
-	if state == 0:
+	#if state == 0:
 		timer.set_wait_time(600)
 	# Right
-	if state == 1:
-		velocity.x = -WALK_SPEED
-		timer.set_wait_time(600)
+	#if state == 1:
+#		velocity.x = -WALK_SPEED
+#		timer.set_wait_time(600)
 	# Left
-	if state == 2:
-		velocity.x = WALK_SPEED
-		timer.set_wait_time(600)
+	#if state == 2:
+#		velocity.x = WALK_SPEED
+#		timer.set_wait_time(600)
 		 # Jump
-	if state == 3 and is_on_floor():
-		velocity.y = -JUMP_SPEED
-		timer.set_wait_time(5)
+	#if state == 3 and is_on_floor():
+#		velocity.y = -JUMP_SPEED
+#		timer.set_wait_time(5)
 		
 		
 	move_and_slide(velocity, Vector2(0, -1))
--- source/Actors/Actor.gd
+++ source/Actors/Actor.gd
@@ -11,6 +11,16 @@ var velocity = Vector2()
 
 
 
+func _process(delta):
+	SkillLoop()
+	
+func SkillLoop():
+	if Input.is_action_just_pressed("shoot"):
+		var spell_instance = bulletPath.instance()
+		spell_instance.position = get_global_position()
+		spell_instance.rotation = get_angle_to(get_global_mouse_position())
+		get_parent().add_child(spell_instance)
+
 func _physics_process(_delta):
 	velocity.y = velocity.y + GRAVITY
 	if velocity.y > 2000:
@@ -30,11 +40,20 @@ func _physics_process(_delta):
 	if Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
 		velocity.x = 0
 	
+<<<<<<< Updated upstream
 	elif Input.is_action_just_pressed("shoot"):
 		shoot()
 		
 		$Node2D.look_at(get_global_mouse_position())
 	
+=======
+	#if Input.is_action_just_pressed("shoot"):
+	#	shoot()
+		
+	#	$Node2D.look_at(get_global_mouse_position())
+	var label = get_node("Label")
+	label.text = str(health) + "/" + str(max_health)
+>>>>>>> Stashed changes
 	move_and_slide(velocity, Vector2(0, -1))
 
 func shoot():
