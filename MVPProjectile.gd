3274c4c9755bf7bd157c03657260412f0a5c6ca1
Tsiegmund <Tyler2023411@live.ytech.edu>
2022-04-29 13:32:43 -0400

    ok 2

--- MVPProjectile.gd
+++ MVPProjectile.gd
@@ -1,19 +1,22 @@
-extends Area2D 
+extends KinematicBody2D 
 
 var velocity = Vector2(0,0)
 var speed = 300
 var maxtime = 2.0
 
-func _physics_process(delta):
-	position += transform.x * speed * delta
-
-func _on_Bullet_body_entered(body):
-	if body.has_method("hit"):
-		body.hit()
-# other stuff below
-	queue_free()
-	
-
 func _ready():
-	connect("body_entered", self, "_on_Bullet_body_entered") 
-	get_node("VisibilityNotifier2D").connect("screen_exited", self, "_on_screen_exited")
+	var timer = Timer.new()
+	timer.wait_time = maxtime
+	timer.autostart = true
+	add_child(timer)
+	timer.connect("timeout", self, "on_timeout")
+
+func on_timeout():
+	self.queue_free()
+
+func _physics_process(delta):
+	
+	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
+	
+	
+		
