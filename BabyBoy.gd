62e8eb2d89a91984761fb57755a41c5ab74f2ba3
John Eckert <90394741+johneckert2023219@users.noreply.github.com>
2022-05-02 10:47:07 -0400

    Mr. Smith view

--- BabyBoy.gd
+++ BabyBoy.gd
@@ -7,9 +7,9 @@ var is_moving_left = true
 
 var gravity = 10
 var velocity = Vector2(0, 0)
-export (float) var max_health = 100
+#export (float) var max_health = 100
 
-onready var health = max_health setget _set_health
+#onready var health = max_health setget _set_health
 
 var speed = 32
 
@@ -31,16 +31,16 @@ func detect_turn_around():
 		is_moving_left = !is_moving_left
 		scale.x = -scale.x
 
-func damage(amount):
-	_set_health(health - amount)
+#func damage(amount):
+#	_set_health(health - amount)
 
-func kill():
-	pass
+#func kill():
+#	pass
 	
-func _set_health(value):
-	health = clamp(value, 0, max_health)
-	if health != prev_health:
-		emit_signal("health_updated", health)
-	if health == 0:
-		kill()
-		emit_signal("killed")
+#func _set_health(value):
+#	health = clamp(value, 0, max_health)
+#	if health != prev_health:
+#		emit_signal("health_updated", health)
+#	if health == 0:
+#		kill()
+#		emit_signal("killed")
