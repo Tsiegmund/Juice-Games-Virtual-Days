fd68f320e21ba2da1108255f89a71f5b596adddc
Tsiegmund <Tyler2023411@live.ytech.edu>
2022-05-25 09:51:25 -0400

    Revert "Merge branch 'Tyler-Branch' into Remote"
    This reverts commit 38dbe836ff77937dfd4b96c38eb0834111809293, reversing
    changes made to fd12a9b234c3bdf1ed084cccdabcf8b51bf942f0.

--- playerDetection.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-func area_shape_entered(area_shape_index):
-	print("something is near")
-	if area_shape_index.is_in_group("player"):
-		get_tree().call_group("enemies", "_on_collision")
-
-
